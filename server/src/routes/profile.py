1f2e4869d82905d31913e90c28ef781d98fa639f
Somnath Das <somnath8das@gmail.com>
2026-06-25 18:19:30 +0530

    old_avatar_remove

--- server/src/routes/profile.py
+++ server/src/routes/profile.py
@@ -1,12 +1,16 @@
+import json
 import os
 from typing import Optional
 import uuid
-
+from fastapi import APIRouter, HTTPException, Depends, File, Form, HTTPException, UploadFile
+from src.database.user.models import User
+from sqlalchemy.orm import Session
+from src.database.database import engine, get_db
 import aiofiles
-from fastapi import FastAPI, File, Form, HTTPException, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi import APIRouter
+
+
+
+
 
 
 UPLOAD_DIR = "uploads"
@@ -24,9 +28,9 @@ router = APIRouter(prefix="/profile", tags=["profile"])
 
 
 
-@router.post("/upload")
+@router.patch("/upload")
 async def upload_photo(file: UploadFile = File(...),
-                       old_avatar_filename: Optional[str] = Form(None),):
+                        user: Optional[str] = Form(None),db: Session = Depends(get_db)):
     if file.content_type not in ALLOWED_CONTENT_TYPES:
         raise HTTPException(status_code=400, detail="Unsupported file type")
 
@@ -54,10 +58,32 @@ async def upload_photo(file: UploadFile = File(...),
             os.remove(filepath)
         raise HTTPException(status_code=500, detail="Failed to save file")
     
-   # delete old avatar if exists
-    if old_avatar_filename:
-        old_path = os.path.join("uploads", str(old_avatar_filename))
-        if os.path.isfile(old_path):
-            os.remove(old_path)
+   
+    
+  
+    user_data = json.loads(user) if user else {}
+    avatar_url = f"{BASE_URL}/uploads/{filename}"
+
+    # save filepath to neon db
+    if user_data:
+        id = user_data.get("id", "")
+        if id:
+            db_user = db.query(User).filter(User.id == id).first()
+        if db_user:
+            db_user.avatar_url = avatar_url # type: ignore
+            db.commit()
+            db.refresh(db_user)
+
+
+
+    # delete old avatar if exists
+    if user_data:
+        old_avatar_url = user_data.get("avatar_url", "")
+        if old_avatar_url:
+            old_avatar_name = old_avatar_url.split("/")[-1]
+            old_path = os.path.join("uploads", old_avatar_name)
+            if os.path.isfile(old_path):
+                os.remove(old_path)
+                # print(f"Deleted old avatar: {old_path}")
 
     return {"url": f"{BASE_URL}/uploads/{filename}"}
