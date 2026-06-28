e3bfb564fc0c3af8345bff38968bc346a22e90e5
Somnath Das <somnath8das@gmail.com>
2026-06-28 12:06:29 +0530

    added drawer

--- AiBlog/src/app/(tabs)/create_blog/_layout.tsx
+++ AiBlog/src/app/(tabs)/create_blog/_layout.tsx
@@ -1,14 +1,30 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Drawer } from "expo-router/drawer";
+import {
+  Drawer,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "expo-router/drawer";
 import { AntDesign } from "@expo/vector-icons";
-import { COLORS } from "@/constants/theme";
+
+function CustomDrawerComponent(props: DrawerContentComponentProps) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <View style={{ padding: 16, alignItems: "center" }}>
+        <AntDesign name="plus-circle" size={24} color="black" />
+      </View>
+    </DrawerContentScrollView>
+  );
+}
 
 const DrawerLayout = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer
+        drawerContent={CustomDrawerComponent}
         screenOptions={{
           drawerActiveTintColor: "blue",
           drawerHideStatusBarOnOpen: true,
